6803a54feb4b84a726ac92a9fb28a9d4feabf1d7
TYCOELECTRONICS\TE497709 <sathvik.bhandar@te.com>
2022-09-01 17:09:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ function App() {
   return (
     <div className="App">
       <nav className='navbar'>
-        <li className='no-bullet'><a href='http://localhost:3000/gateway-onboarding'> Gateway </a></li>
-        <li className='no-bullet'><a href='http://localhost:3000/device-profile'> Device Profile </a></li>
-        <li className='no-bullet'><a href='http://localhost:3000/application'> Application </a></li>
-        <li className='no-bullet'><a href='http://localhost:3000/device-onboarding'> Device </a></li>
+        <li className='no-bullet'><a href='http://ec2-3-110-220-190.ap-south-1.compute.amazonaws.com:3000/gateway-onboarding'> Gateway </a></li>
+        <li className='no-bullet'><a href='http://ec2-3-110-220-190.ap-south-1.compute.amazonaws.com:3000/device-profile'> Device Profile </a></li>
+        <li className='no-bullet'><a href='http://ec2-3-110-220-190.ap-south-1.compute.amazonaws.com:3000/application'> Application </a></li>
+        <li className='no-bullet'><a href='http://ec2-3-110-220-190.ap-south-1.compute.amazonaws.com:3000/device-onboarding'> Device </a></li>
+        <li className='no-bullet'><a href='http://ec2-3-110-220-190.ap-south-1.compute.amazonaws.com:3000/frames'> Frames </a></li>
       </nav>
       <AppRouter />
     </div>
