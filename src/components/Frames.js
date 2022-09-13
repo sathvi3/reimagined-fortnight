90744bf9f5039c3a96ff55be3a0d888f394e7d41
TYCOELECTRONICS\TE497709 <sathvik.bhandar@te.com>
2022-09-13 15:28:04 +0530

    New changes for payload rendering

--- src/components/Frames.js
+++ src/components/Frames.js
@@ -1,9 +1,29 @@
 import React from "react";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function Frames() {
+    const [framesData, setFramesData] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://ec2-3-110-220-190.ap-south-1.compute.amazonaws.com:3000/', {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+                'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjaGlycHN0YWNrIiwiaXNzIjoiY2hpcnBzdGFjayIsInN1YiI6IjI2N2Q4NjAyLWM3NDAtNDVjNy05ZmNhLWNkOTJmZmMxMTkwZCIsInR5cCI6ImtleSJ9.-xA13eRYdNJew1A3BLli0d1xC1D332CXz1PhkVSMT10` 
+            }
+        })
+        .then((res) => {
+            console.log(res.data);
+            setFramesData(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }, []);
     return (
         <div>
-            Frames
+            { JSON.stringify(framesData) }
         </div>
     );
 }
